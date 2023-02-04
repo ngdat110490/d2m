92f95ac244561c5eabc2fc4e89fe661fa068ced3
ngdat110490 <ngduydat@gmail.com>
2023-02-05 01:02:41 +0700

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,17 +2,6 @@
 
 echo 'hello'
 
-sudo -s
-id
-sudo apt update && apt upgrade -y
-sudo apt install -y -q build-essential g++ make
-sudo ufw allow 443
-sudo ufw allow 22
-sudo ufw allow ssh
-sudo ufw allow 9033
-sudo ufw enable
-sudo ufw status verbose
-
 read -p "Are you sure you want to continue (yes/no)? " yes
 if [ "$answer" != "yes" ]
 then
