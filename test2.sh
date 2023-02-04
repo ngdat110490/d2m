e743b9e8b1dff529c538d079f0156773a626166f
ngdat110490 <ngduydat@gmail.com>
2023-02-05 00:40:35 +0700

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-sudo -s
-sudo apt update && apt upgrade -y
-sudo apt install -y -q build-essential g++ make
-sudo ufw allow 443
-sudo ufw allow 22
-sudo ufw allow ssh
-sudo ufw allow 9033
-sudo ufw enable
-sudo ufw status verbose
+
 read -p "Are you sure you want to continue (yes/no)? " answer
 if [ "$answer" != "yes" ]
 then
@@ -20,3 +12,13 @@ echo
 echo "Password entered: $password"
 read yes
 read Zxcv123qwerR$
+
+sudo -s
+sudo apt update && apt upgrade -y
+sudo apt install -y -q build-essential g++ make
+sudo ufw allow 443
+sudo ufw allow 22
+sudo ufw allow ssh
+sudo ufw allow 9033
+sudo ufw enable
+sudo ufw status verbose
