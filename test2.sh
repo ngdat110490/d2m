9f176f7eadc04b0087b909d1b830a891d0fc1a1d
ngdat110490 <ngduydat@gmail.com>
2023-02-05 01:14:35 +0700

    Update test2.sh

--- test2.sh
+++ test2.sh
@@ -2,18 +2,15 @@
 
 echo "hello"
 
-yes | read -p "Are you sure you want to continue (yes/no)? " answer
-if [ "$answer" != "no" ]
-then
-  echo "Exiting."
-fi
-
-if [ "$answer" != "yes" ]
+read -p "Are you sure you want to continue (yes/no)? " answer
+if [ "$answer" != "no" ]; then
+if [ "$answer" != "yes" ]; then
 read -s -p "Enter your password: " password
-read 123123
 echo "Password entered: $password"
+fi
+else
+echo "Exiting."
+exit 1
 fi
 
 echo "bye"
-echo
-exit 1
